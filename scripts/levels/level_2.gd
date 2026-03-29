f49a257fe8d097e367a3ed865dc32f4c05af4a81
Jara <dezvencio@gmail.com>
2026-03-29 18:43:05 +0100

    Day 2 win condition
    Now actually added win condition for day 2

--- scripts/levels/level_2.gd
+++ scripts/levels/level_2.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
 var _is_window_opened: bool = false
+@onready var level_end_trigger: Area2D = $LevelEndTrigger
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	EventBus.window_open.connect(window_opened)
+	level_end_trigger.body_entered.connect(_on_level_end_triggered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func window_opened() -> void:
+	_is_window_opened = true
+
+func _on_level_end_triggered(body: Node2D) -> void:
+	if _is_window_opened and body.is_in_group("cat"):
+		EventBus.day_started.emit(3)
+		SceneLoader.load_scene(Constants.SCENES["DAY_RECAP"])
